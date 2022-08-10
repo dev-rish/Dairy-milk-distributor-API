8a85ff2ce1e3aef6dcb459d3670cd824ca09d51d
Rishabh Singh <rishabh.singh@mapsted.com>
2022-08-10 20:10:22 +0530

    move order routes to separate file

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,10 @@
 const { Router } = require('express');
-const { isNumber } = require('lodash');
-
-const { createOrder } = require('../controllers/order');
-const AppError = require('../utils/appError');
-const wrapHandler = require('../utils/wrapHandler');
+const orderRoutes = require('./order');
 
 const router = Router();
 
 router.get('/', (req, res) => res.send('Milk Distributer'));
 
-router.post('/add', wrapHandler(async (req) => {
-    const { quantity } = req.body;
-
-    if (!isNumber(quantity) || quantity <= 0) {
-        throw new AppError('Invalid quantity', 401);
-    }
-
-    const order = await createOrder(quantity);
-
-    return { statusCode: 201, order };
-}));
+router.use(orderRoutes);
 
 module.exports = router;
