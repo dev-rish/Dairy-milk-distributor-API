f69a6d018ebfa90b8f02143e742354c701f27d67
Rishabh Singh <rishabh.singh@mapsted.com>
2022-08-10 21:58:33 +0530

    add get capacity route

---
+++ routes/capacity.js
@@ -0,0 +1,19 @@
+const { Router } = require('express');
+
+const { getCapacity } = require('../controllers/capacity');
+const AppError = require('../utils/appError');
+const { isValidDate } = require('../utils/helper');
+const wrapHandler = require('../utils/wrapHandler');
+
+const router = Router();
+
+router.get('/checkCapacity/:date', wrapHandler(async (req) => {
+    const { date } = req.params;
+
+    if (!isValidDate(date)) {
+        throw new AppError('Invalid Date', 401);
+    }
+    const capacity = await getCapacity(date);
+
+    return { statusCode: 200, ...capacity };
+}));
