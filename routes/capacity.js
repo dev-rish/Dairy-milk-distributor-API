7f8d54e03abb31578d69ae36c6cc22a872b728d0
Rishabh Singh <rishabh.singh@mapsted.com>
2022-08-11 19:40:29 +0530

    add update capacity route

--- routes/capacity.js
+++ routes/capacity.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
+const moment = require('moment');
 
-const { getCapacity } = require('../controllers/capacity');
+const { getCapacity, updateCapacityDetails } = require('../controllers/capacity');
 const AppError = require('../utils/appError');
+const { DATE_FORMAT } = require('../utils/constants');
 const { isValidDate } = require('../utils/helper');
 const wrapHandler = require('../utils/wrapHandler');
 
@@ -14,6 +16,21 @@ router.get('/checkCapacity/:date', wrapHandler(async (req) => {
         throw new AppError('Invalid Date', 401);
     }
     const capacity = await getCapacity(date);
+
+    return { statusCode: 200, ...capacity };
+}));
+
+router.patch('/updateCapacity/:date', wrapHandler(async (req) => {
+    const { date } = req.params;
+    const { quantityLeft, unitPrice } = req.body;
+
+    const today = moment();
+
+    // prevent capacity updates for past dates
+    if (moment(date, DATE_FORMAT, true).isBefore(today, 'date')) {
+        throw new AppError('Invalid Date', 401);
+    }
+    const capacity = await updateCapacityDetails({ date, quantityLeft, unitPrice });
 
     return { statusCode: 200, ...capacity };
 }));
