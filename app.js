e0e4e9e65fa1cb26005c0e9ca5308c23b7790101
Rishabh Singh <rishabh.singh@mapsted.com>
2022-08-13 00:20:10 +0530

    configure swagger

--- app.js
+++ app.js
@@ -3,25 +3,42 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 
 const routes = require('./routes');
 const globalErrorHandler = require('./utils/globalErrorHandler');
+
+const env = process.env.NODE_ENV;
+const port = process.env.PORT;
+
+const swaggerSpecs = swaggerJsDoc({
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Milk Distributor API',
+            version: '1.0.0',
+            description: 'API to manage, add, update milk orders. Also enable tracking milk capacity left for a particular day',
+        },
+    },
+    apis: ['./models/*.js', './routes/*.js'],
+});
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpecs));
 app.use('/api', routes);
 
 app.use(globalErrorHandler);
 
 mongoose
-    .connect(process.env.MONGODB_URI)
+    .connect(env === 'production' ? process.env.MONGODB_URI_PROD : process.env.MONGODB_URI_DEV)
     .then(() => {
-        const port = process.env.PORT;
         app.listen(port, () => {
             // eslint-disable-next-line no-console
-            console.log(`Server running on port ${port} with ${process.env.NODE_ENV} environment`);
+            console.log(`Server running on port ${port} with ${env} environment`);
         });
     });
